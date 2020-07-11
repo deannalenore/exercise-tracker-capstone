274e32756793ddb8e170c251dc1ccc17993a621e
Deanna Hart <deanna.lenore96@gmail.com>
2020-07-11 13:02:31 -0400

    form functionality working

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import ExerciseLog from "./Components/ExerciseLog";
 function App() {
   return (
     <div className="App">
-      <Jumbotron>
-        <Container>
-          <h1>Exercise Tracker</h1>
-          <p>Keep up with your goals and track your exercise!</p>
-        </Container>
-      </Jumbotron>
       <Nav className="justify-content-end" activeKey="/home">
         <Nav.Item>
           <Nav.Link href="/home">Plan a Workout</Nav.Link>
@@ -24,6 +18,12 @@ function App() {
           <Nav.Link eventKey="link-2">Logout</Nav.Link>
         </Nav.Item>
       </Nav>
+      <Jumbotron>
+        <Container>
+          <h1>Exercise Tracker</h1>
+          <p>Keep up with your goals and track your exercise!</p>
+        </Container>
+      </Jumbotron>
 
       <ExerciseLog />
     </div>
--- src/Components/ExerciseLog.js
+++ src/Components/ExerciseLog.js
@@ -2,29 +2,62 @@ import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 
 export class ExerciseLog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+      exercise: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    console.log(event.target);
+  };
+
   render() {
     return (
       <div>
         <h1>Daily Exercise Log</h1>
-        <Form>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Date</Form.Label>
-            <Form.Control type="date" placeholder="Enter Date" />
-          </Form.Group>
-          <Form.Group controlId="exampleForm.ControlSelect1">
-            <Form.Label>Exercise</Form.Label>
-            <Form.Control as="select">
-              <option>Cardio</option>
-              <option>Running</option>
-              <option>Walking</option>
-              <option>Weight Training</option>
-              <option>Swimming</option>
-            </Form.Control>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        <div ClassName="form">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Date</Form.Label>
+              <Form.Control
+                onChange={this.handleChange}
+                value={this.state.date}
+                type="date"
+                name="date"
+                placeholder="Enter Date"
+              />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Exercise</Form.Label>
+              <Form.Control
+                as="select"
+                name="exercise"
+                onChange={this.handleChange}
+                value={this.state.exercise}
+              >
+                <option value="cardio">Cardio</option>
+                <option value="running">Running</option>
+                <option value="walking">Walking</option>
+                <option value="weight training">Weight Training</option>
+                <option value="swimming">Swimming</option>
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
